bb8912a36a090f57cd8d17d62209da3c7b8d44dd
anton <antondanilin@gmail.com>
2023-11-08 22:28:19 +0300

    multi-digit numbers and some validation

--- calculator_project/calculator_template.php
+++ calculator_project/calculator_template.php
@@ -1,23 +1,53 @@
 <?php
+$operator = $_GET['op'] . $_GET['operator'] ?? '';
+$digit = $_GET['digit'] ?? '';
+$equalitySign = $_GET['calc'] ?? '';
+$calculation = null;
+
 $num1 = $_GET['num1'] ?? '';
 $num2 = $_GET['num2'] ?? '';
-$operator = $_GET['operator'] ?? '';
-$result = '';
 
-$formula = ''; // необходимо заменить на получение актуальной формулы
+$num1.= ($operator === '') ? $digit : '';
+$num2.= ($operator != '') ? $digit : '';
+
+if ($equalitySign === '='){
+    switch ($operator) {
+        case '+':
+            $calculation = (int)$num1 + (int)$num2;
+            break;
+        case '-':
+            $calculation = (int)$num1 - (int)$num2;
+            break;
+        case '*':
+            $calculation = (int)$num1 * (int)$num2;
+            break;
+        case '/':
+            if ((int)$num2 !== 0) {
+                $calculation = (int)$num1 / (int)$num2;
+            } else {
+                $calculation = "Division by zero is not allowed";
+            }
+            break;
+        default:
+            $calculation = "Invalid operator";
+            break;
+    }
+}
+
+$output = $num1 . $operator . $num2 . $equalitySign . $calculation; // необходимо заменить на получение актуальной формулы
 
 ?>
 <table>
     <tr>
         <td>
-            <form>
+            <form action="calculator_template.php" method="get">
                 <input type="hidden" name="num1" value="<?= htmlspecialchars($num1) ?>">
                 <input type="hidden" name="num2" value="<?= htmlspecialchars($num2) ?>">
                 <input type="hidden" name="operator" value="<?= htmlspecialchars($operator) ?>">
                 <table>
                     <tr>
                         <td colspan="4">
-                            <input type="text" readonly="true" value="<?= htmlspecialchars($formula) ?>" >
+                            <input type="text" value="<?= htmlspecialchars($output) ?>" readonly="true">
                         </td>
                     </tr>
 
